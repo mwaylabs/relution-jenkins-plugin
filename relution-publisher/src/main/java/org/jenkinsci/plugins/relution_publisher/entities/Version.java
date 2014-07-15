94180b94cff801938d09911d9549412237a3a390
Markus Pfeiffer <m.pfeiffer@mwaysolutions.com>
2014-07-15 16:31:22 +0200

    Updated copyright notices/Added missing copyright notices.

--- relution-publisher/src/main/java/org/jenkinsci/plugins/relution_publisher/entities/Version.java
+++ relution-publisher/src/main/java/org/jenkinsci/plugins/relution_publisher/entities/Version.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 M-Way Solutions GmbH
+ * Copyright (c) 2013-2014 M-Way Solutions GmbH
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
